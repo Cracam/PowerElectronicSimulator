d564d883c316c9c37be8a13220ebfad5b21cbefd
Administrateur <Administrateur@192.168.0.16>
2024-10-18 17:11:17 +0200

    Firt update

---
+++ src/Componement/Componement.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Componement;
+
+/**
+ * This class will represent a componement (after being processed by the circuit processing)
+ * Thoses componement will be of 3 main types : 
+ *  - Passives ones
+ *  - Voltage / Current Sources
+ *  - Active (power electronic componement)
+ * @author LECOURT Camille
+ */
+public abstract class Componement {
+    
+    
+}
