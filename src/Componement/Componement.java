06fc623d10a3cf55e407ce7b11d3437112b5d37e
LECOURT Camille <cracaml72@gmail.com>
2024-10-19 11:46:10 +0200

    First update

--- src/Componement/Componement.java
+++ src/Componement/Componement.java
@@ -12,11 +12,43 @@ package Componement;
  *  - Active (power electronic componement)
  * @author LECOURT Camille
  */
-public abstract class Componement {
+public class Componement {
+    private String name;
+    private int imputNode;
+    private int outputNode;
+    private String type;
+    private String number;
     
-    public Componement(){
+    /**
+     * This is the builder of the componement
+     * 
+     * @param name / name of the componement (we will process type and number variable)
+     * @param imputNode / name of the imput node for this componement
+     * @param outputNode  / name of the output node for this componement
+     */
+    public Componement(String name,int imputNode,int outputNode){
+        this.name=name;
+        this.imputNode=imputNode;
+        this.outputNode=outputNode;
         
         
+        this.type = "";
+        this.number = "";
+
+        for (char c : name.toCharArray()) {
+            
+            if (Character.isDigit(c)) {
+                this.number += c;
+            } else {
+                this.type += c;
+            }
+        }
     }
+ 
+   
+
+    
+    
+    
     
 }
